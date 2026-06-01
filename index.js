97b1d87e723d0ff1ff7744e9a668fde150dc5a91
kuochen-peng <abswang1345@yahoo.com.tw>
2026-06-01 23:34:37 +0800

    給cron-job.org測試用

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ import attendanceRouter from './routes/attendance.js'
 import leaveRequestRouter from './routes/leaveRequest.js'
 
 mongoose
-	.connect(process.env.DB_URL)
-	.then(() => {
-		console.log('資料庫連線成功')
-	})
-	.catch((error) => {
-		console.log('資料庫連線失敗')
-		console.log(error)
-	})
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('資料庫連線成功')
+  })
+  .catch((error) => {
+    console.log('資料庫連線失敗')
+    console.log(error)
+  })
 
 const app = express()
 
@@ -24,9 +24,13 @@ app.use(cors())
 
 app.use(express.json())
 app.use((err, req, res, _next) => {
-	res.status(StatusCodes.BAD_REQUEST).json({
-		message: '資料格式錯誤',
-	})
+  res.status(StatusCodes.BAD_REQUEST).json({
+    message: '資料格式錯誤',
+  })
+})
+
+app.get('/', (req, res) => {
+  res.status(200).send('ok')
 })
 
 app.use('/user', userRouter)
@@ -34,5 +38,5 @@ app.use('/attendance', attendanceRouter)
 app.use('/leaveRequest', leaveRequestRouter)
 
 app.listen(process.env.PORT || 4000, () => {
-	console.log('伺服器啟動 http://localhost:4000')
+  console.log('伺服器啟動 http://localhost:4000')
 })
